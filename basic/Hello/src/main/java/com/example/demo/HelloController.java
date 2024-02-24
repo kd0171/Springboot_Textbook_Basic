4610df131faac276e68347faf94c33070324591c
kd0171 <kido.0117.1895@gmail.com>
2024-02-24 03:08:20 +0100

    first commit

---
+++ basic/Hello/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World! changed";
+    }
+}
