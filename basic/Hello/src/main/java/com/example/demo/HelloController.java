1c8468fc104bf65eed645d4875c026c8a4771768
kd0171 <kido.0117.1895@gmail.com>
2024-02-24 04:41:51 +0100

    thymeleafを用いる

--- basic/Hello/src/main/java/com/example/demo/HelloController.java
+++ basic/Hello/src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.servlet.*;
 
-@RestController
+@Controller
 public class HelloController {
-	@PostMapping("/hello2")
-	public String sayHello(@RequestParam("name2") String name) {
-	    return "Hello, World! changed" + "こんにちは" + name + "さん！";
+	@GetMapping("/hello2")
+	public ModelAndView sayHello(@RequestParam("name") String name, ModelAndView mv) {
+	    mv.setViewName("Hello");
+	    // ()内は、templatesのフォルダ内のファイル名（＋.html）を指す。
+	    mv.addObject("name",name);
+	    // ()内は、このビューが用いるデータを指す
+		return mv;
 	}
 }
